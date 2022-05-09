7bec00004162b116ca0b992b53c8abf19be404d4
Andrei-Ioan Debu <andreidebu13@icloud.com>
2022-05-09 23:35:57 +0300

    Project Sedan update

--- ProjectOOP/Sedan.cpp
+++ ProjectOOP/Sedan.cpp
@@ -1,1 +1,57 @@
 #include "Sedan.h"
+
+Sedan::Sedan(int id, string name, string model, int hp, int km, int engine, int price, Types type) :
+	Car(id, name, model, hp, km, engine, price), c_type{ type }
+{
+	this->c_acceleration = 0;
+	this->c_comfort = false;
+	this->c_type = type;
+}
+
+// getters
+
+int Sedan::getAcceleration()
+{
+	return this->c_acceleration;
+}
+
+bool Sedan::isComfort()
+{
+	return this->c_comfort;
+}
+
+Types Sedan::getType()
+{
+	return this->c_type;
+}
+
+// setters
+
+void Sedan::setAcceleration(int newAcceleration)
+{
+	this->c_acceleration = newAcceleration;
+}
+
+void Sedan::setComfort(bool newComfort)
+{
+	this->c_comfort = newComfort;
+}
+
+void Sedan::setType(Types newType)
+{
+	this->c_type = newType;
+}
+
+void Sedan::display(ostream& os) const
+{
+	os << "Sedan ";
+	Car::display(os);
+	if (this->c_comfort == true)
+	{
+		os << " comfortable";
+	}
+	else
+	{
+		os << " uncomfortable";
+	}
+}
--- ProjectOOP/Sedan.h
+++ ProjectOOP/Sedan.h
@@ -1,5 +1,30 @@
 #pragma once
-class Sedan
-{
+#include "Car.h"
+#include <iostream>
+
+enum class Types { // note: friend ostream& operator <<
+	LUXURY,
+	AGGRESSIVE,
 };
 
+class Sedan :
+	public Car
+{
+private:
+	int c_acceleration;
+	bool c_comfort;
+	Types c_type;
+
+public:
+	Sedan(int id, string name, string model, int hp, int km, int engine, int price, Types type);
+
+	int getAcceleration();
+	bool isComfort();
+	Types getType();
+
+	void setAcceleration(int);
+	void setComfort(bool);
+	void setType(Types);
+
+	void display(ostream& os) const override;
+};
