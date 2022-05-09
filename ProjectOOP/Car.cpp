7a83a7434307df757c2eb3ea04c80af6b630aefc
Andrei-Ioan Debu <andreidebu13@icloud.com>
2022-05-09 23:34:14 +0300

    Project Car update

--- ProjectOOP/Car.cpp
+++ ProjectOOP/Car.cpp
@@ -1,1 +1,85 @@
 #include "Car.h"
+
+Car::Car(int id, string name, string model, int hp, int km, int engine, int price)
+{
+	this->c_id = id;
+	this->c_name = name;
+	this->c_model = model;
+	this->c_hp = hp;
+	this->c_km = km;
+	this->c_engine = engine;
+	this->c_price = price;
+}
+
+// getters
+
+int Car::getID()
+{
+	return this->c_id;
+}
+string Car::getName()
+{
+	return this->c_name;
+}
+string Car::getModel()
+{
+	return this->c_model;
+}
+int Car::getHP()
+{
+	return this->c_hp;
+}
+int Car::getKM()
+{
+	return this->c_km;
+}
+int Car::getEngine()
+{
+	return this->c_engine;
+}
+int Car::getPrice()
+{
+	return this->c_price;
+}
+
+// setters
+
+void Car::setID(int newID)
+{
+	this->c_id = newID;
+}
+void Car::setName(string newName)
+{
+	this->c_name = newName;
+}
+void Car::setModel(string modelName)
+{
+	this->c_model = modelName;
+}
+void Car::setHP(int newHP)
+{
+	this->c_hp = newHP;
+}
+void Car::setKM(int newKM)
+{
+	this->c_km = newKM;
+}
+void Car::setEngine(int newEngine)
+{
+	this->c_engine = newEngine;
+}
+void Car::setPrice(int newPrice)
+{
+	this->c_price = newPrice;
+}
+
+void Car::display(ostream& os) const
+{
+	os << "#" << c_id << " " << c_name << " " << c_model << " with " << c_hp << " horsepower, " << c_km << " kilometres, " << c_engine << " engine capacity and costs " << c_price;
+}
+
+ostream& operator<<(ostream& os, const Car& c)
+{
+	c.display(os);
+	return os;
+}
--- ProjectOOP/Car.h
+++ ProjectOOP/Car.h
@@ -1,5 +1,41 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Car
 {
-};
+private:
+	int c_id;
+protected:
+	string c_name;
+	string c_model;
+	int c_hp; // number of horsepower of the car
+	int c_km;
+	int c_engine; // size of the engine
+	int c_price; // number of doors
 
+public:
+	Car(int id, string name, string model, int hp, int km, int engine, int price);
+
+	int getID(); // getters
+	string getName();
+	string getModel();
+	int getHP();
+	int getKM();
+	int getEngine();
+	int getPrice();
+
+	void setID(int); // setters
+	void setName(string);
+	void setModel(string modelName);
+	void setHP(int);
+	void setKM(int);
+	void setEngine(int);
+	void setPrice(int);
+
+	virtual void display(ostream& os) const;
+	friend ostream& operator <<(ostream& os, const Car& c);
+
+};
