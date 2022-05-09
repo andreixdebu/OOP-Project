d5dc16688ac291b3d03fd93650fbaa5f6ff2ad75
Andrei-Ioan Debu <andreidebu13@icloud.com>
2022-05-09 23:37:04 +0300

    Project SUV update

--- ProjectOOP/SUV.cpp
+++ ProjectOOP/SUV.cpp
@@ -1,1 +1,53 @@
 #include "SUV.h"
+
+SUV::SUV(int id, string name, string model, int hp, int km, int engine, int price, float height, int doors) :
+	Car(id, name, model, hp, km, engine, price), c_height{ height }, c_doors{ doors }
+{
+	this->c_height = height;
+	this->c_doors = 5;
+	this->c_comfort = false;
+}
+
+// getters
+
+float SUV::getHeight()
+{
+	return this->c_height;
+}
+int SUV::getDoors()
+{
+	return this->c_doors;
+}
+bool SUV::isComfort()
+{
+	return this->c_comfort;
+}
+
+// setters
+
+void SUV::setHeight(float newHeight)
+{
+	this->c_height = newHeight;
+}
+void SUV::setDoors(int newDoors)
+{
+	this->c_doors = newDoors;
+}
+void SUV::setComfort(bool newComfort)
+{
+	this->c_comfort = newComfort;
+}
+
+void SUV::display(ostream& os) const
+{
+	os << "SUV ";
+	Car::display(os);
+	if (this->c_comfort)
+	{
+		os << " comfortable";
+	}
+	else
+	{
+		os << " uncomfortable";
+	}
+}
--- ProjectOOP/SUV.h
+++ ProjectOOP/SUV.h
@@ -1,5 +1,26 @@
 #pragma once
-class SUV
-{
-};
+#include "Car.h"
+#include <iostream>
+#include <string>
 
+class SUV :
+	public Car
+{
+private:
+	float c_height;
+	int c_doors;
+	bool c_comfort;
+
+public:
+	SUV(int id, string name, string model, int hp, int km, int engine, int price, float height, int doors);
+
+	float getHeight();
+	int getDoors(); // number of doors
+	bool isComfort();
+
+	void setHeight(float);
+	void setDoors(int);
+	void setComfort(bool);
+
+	void display(ostream& os) const override;
+};
