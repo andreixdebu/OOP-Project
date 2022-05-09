7e39cddd37bba8cd42800bcb3078bd63d960aad6
Andrei-Ioan Debu <andreidebu13@icloud.com>
2022-05-09 23:36:34 +0300

    Project Sport update

--- ProjectOOP/Sport.cpp
+++ ProjectOOP/Sport.cpp
@@ -1,1 +1,55 @@
 #include "Sport.h"
+
+Sport::Sport(int id, string name, string model, int hp, int km, int engine, int price, float height) :
+	Car(id, name, model, hp, km, engine, price), c_height{ height }
+{
+	this->c_acceleration = 0;
+	this->c_comfort = false;
+	this->c_height = height;
+}
+
+// getters
+
+int Sport::getAcceleration()
+{
+	return this->c_acceleration;
+}
+bool Sport::isComfort()
+{
+	return this->c_comfort;
+}
+float Sport::getHeight()
+{
+	return this->c_height;
+}
+
+// setters
+
+void Sport::setAcceleration(int newAcceleration)
+{
+	this->c_acceleration = newAcceleration;
+}
+
+void Sport::setComfort(bool newComfort)
+{
+	this->c_comfort = newComfort;
+}
+
+void Sport::setHeight(float newHeight)
+{
+	this->c_height = newHeight;
+}
+
+void Sport::display(ostream& os) const
+{
+	os << "Sport ";
+	Car::display(os);
+	if (this->c_comfort)
+	{
+		os << " comfortable";
+	}
+	else
+	{
+		os << " uncomfortable";
+	}
+}
--- ProjectOOP/Sport.h
+++ ProjectOOP/Sport.h
@@ -1,5 +1,24 @@
 #pragma once
-class Sport
-{
-};
+#include "Car.h"
 
+class Sport :
+	public Car
+{
+private:
+	int c_acceleration;
+	bool c_comfort;
+	float c_height;
+
+public:
+	Sport(int id, string name, string model, int hp, int km, int engine, int price, float height);
+
+	int getAcceleration();
+	bool isComfort();
+	float getHeight();
+
+	void setAcceleration(int); // 100 km/h in seconds
+	void setComfort(bool);
+	void setHeight(float); // in cm
+
+	void display(ostream& os) const override;
+};
